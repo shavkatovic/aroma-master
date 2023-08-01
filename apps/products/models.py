85b2895ef754465b0e573e239929d9f11f451b9f
yevgenevic <tannagashevalisher07@gmail.com>
2023-08-01 10:57:07 +0500

    yangilandi

--- apps/products/models.py
+++ apps/products/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
+
 # Create your models here.
+
+
+class Comments(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.EmailField()
+    subject = models.CharField(max_length=100)
+    message = models.TextField()
+    rate = models.IntegerField()
+    date = models.DateField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
+
